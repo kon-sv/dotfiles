8729350020af233dd9118629887c1eeb84d905cb
kon-sv <kon.sv@outlook.com>
2023-04-29 09:20:56 +0300

    prinzeugen

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -2,4 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+$HOME/scripts/ascii/select.sh (random 1 11) &
+
+set PATH $PATH:$HOME/.local/bin
+
 starship init fish | source
--- scripts/ascii/executable_select.sh
+++ scripts/ascii/executable_select.sh
@@ -3,12 +3,14 @@
 # Specify the section number to print
 section_num=$1
 
+FP=$HOME/scripts/ascii/anime.txt
+
 # Read the file into an array
-mapfile -t file_lines < anime.txt
+mapfile -t file_lines < $FP
 
 # Find the start and end line numbers of the specified section
-start_line=$(grep -n "#$section_num$" anime.txt | cut -d ":" -f1)
-end_line=$(grep -n "#$((section_num+1))$" anime.txt | cut -d ":" -f1)
+start_line=$(grep -n "#$section_num$" $FP | cut -d ":" -f1)
+end_line=$(grep -n "#$((section_num+1))$" $FP | cut -d ":" -f1)
 
 # Print out the section
 if [[ -n "$start_line" && -n "$end_line" ]]; then
